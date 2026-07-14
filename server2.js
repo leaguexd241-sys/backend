c58ebf30a6ec0cd934c5b9a097990740246fe7dd
leaguexd241-sys <leaguexd241@gmail.com>
2026-07-14 01:33:40 -0500

    Update server2.js

--- server2.js
+++ server2.js
@@ -27,7 +27,7 @@ const compression = require('compression');
 const keystore = require('./keystore');
 
 // --- Configuración Principal ---
-const PORT = parseInt(process.env.PORT || '3001', 10);
+const PORT = parseInt(process.env.PORT || '3000', 10);
 const MONGO = process.env.MONGO_URL || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/grassland';
 const JWT_SECRET = process.env.JWT_SECRET || crypto.randomBytes(64).toString('hex');
 const ACCESS_TOKEN_EXPIRES = process.env.ACCESS_EXPIRES || '15m';
